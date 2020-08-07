7a3c46483d2d0fc25b2604656dcb418b06c78b9d
damonbogich <damonbogich@gmail.com>
2020-08-07 13:59:29 -0400

    working on number 2

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,38 @@
+#1. populate dictionary: {value: (index, difference)}
+#2. convert dict into list (index = (value, (differrence, index)))
+
+
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    dictt = {}
+    #populate dict with (val, (index, difference))
+    for i in range(len(weights)):
+      if (weights[i] in dictt) and (weights[i] == dictt[weights[i]][1]):
+        return (i,dictt[weights[i]][0])
+      dictt[weights[i]] = (i,limit - weights[i])
 
+
+    #reverse order so that highest index up front
+    sorted_dictt = sorted(dictt.items(), reverse=True, key=lambda x: x[1][0])
+    print('sorted dictt', sorted_dictt)
+
+    #loop through reversed list to and check if difference in dict
+
+    # check if              k         k         k            k       dicct{k}
+    # sorted_dictt = [(7,(3,2)), (5,(2,4)), (4, (1,5)),  (10, (0,-1))]
+    # dictt= {4: (1,5), 5: (2,4), 7: (3,2), 10: {0, -1}}
+    
+    for item in sorted_dictt:
+      if item[1][1] in dictt:
+        answer = (item[1][0],dictt[item[1][1]][0])
+        print('here',dictt[item[1][1]][0])
+        return answer
+    
     return None
+
+weights = [10,4,5,7]
+length = 4
+limit = 9
+get_indices_of_item_weights(weights, length, limit)
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,38 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    dictt = {}
 
-    return route
+    for ticket in tickets:
+      dictt[ticket.source] = ticket.destination
+    #put in first fligth
+    final_list = [dictt['NONE']]
+
+
+    for i in range(len(tickets)-3):
+        #get destination for where you arrived
+      next_trip = dictt[final_list[i]]
+
+      final_list.append(next_trip)
+
+      if i == len(tickets)-3:
+        final_list.append('NONE')
+
+    return final_list
+
+
+ticket_1 = Ticket("PIT", "ORD")
+ticket_2 = Ticket("XNA", "SAP")
+ticket_3 = Ticket("SFO", "BHM")
+ticket_4 = Ticket("FLG", "XNA")
+ticket_5 = Ticket("NONE", "LAX")
+ticket_6 = Ticket("LAX", "SFO")
+ticket_7 = Ticket("SAP", "SLC")
+ticket_8 = Ticket("ORD", "NONE")
+ticket_9 = Ticket("SLC", "PIT")
+ticket_10 = Ticket("BHM", "FLG")
+
+tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+            ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+
+print(reconstruct_trip(tickets, 10))
